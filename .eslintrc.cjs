77fc576e64f7253d905c4486200e54e9e99581b8
delalamo <diego.delalamo@gmail.com>
2025-04-14 09:55:28 +0200

    Additional pre-commits

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+};
