1de4202ea4f26ab51af051df72a2a5449636a4d9
delalamo <diego.delalamo@gmail.com>
2025-04-15 18:46:14 +0200

    Adding some blog entries

---
+++ src/blog-posts/content/2025-03-07-denovo-mabs.jsx
@@ -0,0 +1,39 @@
+// src/blog-posts/content/2025-03-07-antibody-design.jsx
+import React from 'react';
+
+export const post = {
+  id: '2025-03-07-antibody-design', // Generated ID based on date and title
+  title: 'Antibody design de novo vs in vivo', // From frontmatter
+  date: '2025-03-07', // From frontmatter date field
+  category: 'Bio/ML', // As requested
+  excerpt: 'The Baker lab tackles <em>de novo</em> antibody design by narrowing the problem as much as possible.', // First paragraph
+  content: (
+    <>
+      <p>The Baker lab tackles <em>de novo</em> antibody design by narrowing the problem as much as possible.</p>
+
+      <p>Last week saw a sizable update to the RF-antibody preprint<sup><a href="#fn1" id="fnref1">1</a></sup> as well as the release of its source code. RF-antibody repurposed the backbone design model RF-diffusion<sup><a href="#fn2" id="fnref2">2</a></sup> to create single-chain antibodies that bound with some success to some previously validated targets, such as toxin B from <em>C. difficile</em> and hemagglutinin from influenza. Coming less than one year after the publication of vanilla RF-diffusion, the project initially made a considerable splash, and coincided with the announcement of a <a href="https://www.fiercebiotech.com/biotech/new-ai-drug-discovery-powerhouse-xaira-rises-1b-funding">spin-off startup</a>, which hired the researchers behind the report. Whereas the first version focused only on VHHs, The updated version includes details on the design of single-chain variable fragment (scFvs), some retrospective binder validation with AlphaFold3<sup><a href="#fn3" id="fnref3">3</a></sup>, and most importantly, the source code.</p>
+
+      <p>Looking back on the preprint, it's clear that some of the success can be attributed to how the authors managed to constrain the problem of <em>de novo</em> antibody design, which is defined by the huge number of potentially viable sequences (estimates put it as high 10<sup>18</sup> possible antibodies<sup><a href="#fn4" id="fnref4">4</a>,<a href="#fn5" id="fnref5">5</a></sup>). The adaptive immune systems of jawed vertebrates navigate this space using something akin to a combination of high-throughput screening and directed evolution: infection exposes the antigen(s) to millions or billions of naive germline antibodies that vary in chain and CDR composition, and successful binders are then affinity-matured in a Darwinian process called somatic hypermutation. In effect, this system constrains the problem by limiting the search to promising germline lineages that already bind to their target, and the fact that nearly identical antibodies routinely show up in different humans attests to the influence these constraints have on the outcome<sup><a href="#fn6" id="fnref6">6</a></sup>.</p>
+
+      <p>Now contrast this with RF-antibody, which limits the antibody design problem by massively reducing the number of design parameters. Whereas natural germlines have different sequence compositions and CDR lengths, RF-antibody exclusively tests VHHs and scFvs with one hand-picked framework each (h-NbBcII10FGLA and trastuzumab/Herceptin, respectively) and a fixed set of CDR lengths. The users then pre-specify the epitope, leaving the algorithm to sort out docking, CDR conformation design, and CDR sequence design. Their workflows couples the first two of these steps, which were the attention-getting steps from the workflow, while leaving the third for later.</p>
+
+      <p>It is at this third step that the sequence space can be more thoroughly explored than what would ordinarily happen in the immune system. Their sequence design method of choice, ProteinMPNN<sup><a href="#fn7" id="fnref7">7</a></sup>, tends to design CDR sequences that look nothing like antibodies<sup><a href="#fn8" id="fnref8">8</a></sup> (we have a preprint coming out soon that goes into more detail about this). On the one hand, this simplifies the problem considerably since only the residues in the CDRs are being designed. On the other hand, it massively expands the search space, as ProteinMPNN designs CDRs that aren't constrained by the processes defining our adaptive immune systems<sup><a href="#fn9" id="fnref9">9</a></sup>. The authors acknowledge as much, stating in their discussion that "designing sequences that more closely match human CDR sequences would be expected to reduce the potential immunogenicity and developability". In any case, if the authors intended for this pipeline to be a minimum viable product, then they succeeded at that by identifying binders in the first place, and can now tackle the harder problem of turning them into drugs. I suspect that, as the brakes are applied to ProteinMPNN during CDR sequence design in service of that objective, the reduction in diversity will be counterbalanced by considering other frameworks and CDR lengths. Over time, this may result in a broader pool of potential binders more reflective of what the human adaptive immune system is capable of producing.</p>
+
+      <section className="footnotes">
+        <hr />
+        <ol>
+          <li id="fn1">Bennett et al, <a href="https://doi.org/10.1101/2024.03.14.585103">"Atomically accurate de novo design of antibodies with RFdiffusion"</a> biorxiv 2024 <a href="#fnref1" title="Jump back to footnote 1 in the text">↩</a></li>
+          <li id="fn2">Watson et al, <a href="https://doi.org/10.1038/s41586-023-06415-8">"De novo design of protein structure and function with RFdiffusion"</a> Nature 2023 <a href="#fnref2" title="Jump back to footnote 2 in the text">↩</a></li>
+          <li id="fn3">Abramson et al, <a href="https://doi.org/10.1038/s41586-024-07487-w">"Accurate structure prediction of biomolecular interactions with AlphaFold 3"</a> Nature 2024 <a href="#fnref3" title="Jump back to footnote 3 in the text">↩</a></li>
+          <li id="fn4">Janeway et al, <a href="https://www.ncbi.nlm.nih.gov/books/NBK10775/">"Immunobiology: The Immune System in Health and Disease (5th edition)"</a> Garland Science 2001 <a href="#fnref4" title="Jump back to footnote 4 in the text">↩</a></li>
+          <li id="fn5">Rees, <a href="https://doi.org/10.1080/19420862.2020.1729683">"Understanding the human antibody repertoire"</a> mAbs 2019 <a href="#fnref5" title="Jump back to footnote 5 in the text">↩</a></li>
+          <li id="fn6">Wang et al, <a href="https://doi.org/10.1016/j.immuni.2022.03.019">"A large-scale systematic survey reveals recurring molecular features of public antibody responses to SARS-CoV-2"</a> Immunity 2022 <a href="#fnref6" title="Jump back to footnote 6 in the text">↩</a></li>
+          {/* Note: Corrected the malformed DOI link from the original markdown for footnote 7 */}
+          <li id="fn7">Dauparas et al, <a href="https://doi.org/10.1126/science.add2187">“Robust deep learning-based protein sequence design using ProteinMPNN”</a> Science 2022 <a href="#fnref7" title="Jump back to footnote 7 in the text">↩</a></li>
+          <li id="fn8">del Alamo et al, <a href="https://cdn-api.swapcard.com/public/files/caa2cb897fb3417c8ebb6aa210abf28d.pdf">"Antibody CDR design by ensembling inverse folding with protein language models"</a> PEGS Boston 2024 <a href="#fnref8" title="Jump back to footnote 8 in the text">↩</a></li>
+          <li id="fn9">Chinnery et al, <a href="https://doi.org/10.1101/2024.03.26.586756">"Baselining the Buzz Trastuzumab-HER2 Affinity, and Beyond"</a> biorxiv 2024 <a href="#fnref9" title="Jump back to footnote 9 in the text">↩</a></li>
+        </ol>
+      </section>
+    </>
+  ),
+};
---
+++ src/blog-posts/content/2025-03-16-conf_flexibility.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+export const post = {
+  id: '2025-03-14-conf-sampling',
+  title: 'Training data for conformational flexibility prediction',
+  date: '2025-03-14',
+  // Add the category field
+  category: 'Bio/ML',
+  excerpt: 'How much data from molecular dynamics simulations are needed to predict protein flexibility?',
+  content: (
+    <>
+      {/* This paragraph seems redundant with the title/excerpt, consider removing */}
+      <p>How much data from molecular dynamics simulations are needed to predict protein flexibility?</p> 
+
+      <p>The loadedness of the term "protein dynamics" complicates many attempts at figuring out what scientists want exactly when using modern AI/ML tools to predict protein structural heterogeneity. For the two to three years when AlphaFold2 and related methods were the state of the art, modeling dynamics mostly meant hacking these methods to yield some conformational heterogeneity<sup><a href="#fn1" id="fnref1">1</a></sup>. In the last year and a half or so, however, several high-quality neural networks have been released that tackle this problem more directly, at various levels of comprehensiveness: AlphaFlow<sup><a href="#fn2" id="fnref2">2</a></sup>, BioEmu<sup><a href="#fn3" id="fnref3">3</a></sup>, and Str2str<sup><a href="#fn4" id="fnref4">4</a></sup> being some examples.</p>
+
+      {/* ... other paragraphs ... */}
+
+      <p>In general it seems that two types of neural networks have emerged to fill this gap. The first learns from simulation data for individual proteins and attempts to, in effect, learn fundamental properties about those systems by interpolating them (example <a href="https://delalamo.github.io/papers/2025/02/24/rmd.html">here</a>). The second try to be more generalist, and learn from huge quantities of structural and MD data.</p>
+
+      <p>Earlier this week a preprint was released that attempts to do the second <sup><a href="#fn7" id="fnref7">7</a></sup>. The method, sAMt, uses a variational autoencoder and an input structure as a template to model proteins heterogeneity from sequence alone.</p>
+
+      <img
+        src="/assets/post_images/2025_03_14_A.png"
+        alt="Overview of the network from Janson et al"
+        width="700" // Keep your desired width (or use style={{ width: '600px' }})
+        style={{
+          display: 'block',    // Make the image a block element
+          marginLeft: 'auto',  // Auto margin on the left
+          marginRight: 'auto', // Auto margin on the right
+          height: 'auto'       // Maintain aspect ratio (optional if using width attribute)
+          // If using style for width: width: '600px'
+        }}
+      />
+
+      <p>There are many things to like about this preprint - unlike the method I discussed before, it uses more modern neural network architectures such as the AlphaFold structure module<sup><a href="#fn8" id="fnref8">8</a></sup> and diffusion models, as well as the frame aligned point error loss function. But one thing stood out in this document that warrants further discussion:</p>
+
+      <blockquote>
+      <p>"...free energy values from aSAMt were not in accurate quantitative agreement with the long MD results indicating that aSAMt learned the extent of conformational sampling covering a broad range of conformations but struggles in correctly reproducing relative probabilities between different states . This result may be expected, since the training data from mdCATH were simulations at most 500 ns long with few transitions between alternate states at a given temperature from which relative probabilities could have been learned..."</p>
+      </blockquote>
+
+      <p>In contrast, the long MD simulations against which the conformers are compared last one or more milliseconds<sup><a href="#fn9" id="fnref9">9</a></sup>. That's approximately 2,000 longer than the training examples. Moreover, the modeled proteins were explicitly chosen for their ability to quickly fold and unfold, suggesting a degree of comprehensiveness in the simulations. In contrast, the proteins in mdCATH are simply short simulations of a cross-section of structural diversity<sup><a href="#fn10" id="fnref10">10</a></sup>.</p>
+
+      <p>This point has been raised in the past in verbal discussions on AlphaFlow. The motions captured in mdCATH are, in effect, Brownian motion, which is neither difficult nor interesting to predict. The value added by a neural network capable of predicting certain properties of protein dynamics would come from, at a minimum, a capacity to predict the weighted distribution of different conformations. Thus far, BioEmu is the closest to achieving this, although its accurate predictions are limited to conformational breadth and folded versus unfolded states. That said, there's no reason that further progress in this field will unlock this capability to learn from the short MD simulations we have available.</p>
+
+      {/* Note: Using 'class' might give a warning in React. Use 'className' instead. */}
+      <section className="footnotes"> 
+        {/* Corrected hr tag */}
+        <hr /> 
+        <ol>
+          <li id="fn1">Sala et al <a href="https://doi.org/10.1016/j.sbi.2023.102645">"Modeling conformational states of proteins with AlphaFold"</a> Curr Opin Struct Biol 2023 <a href="#fnref1" title="Jump back to footnote 1 in the text">↩</a></li>
+          <li id="fn2">Lewis et al <a href="https://doi.org/10.1101/2024.12.05.626885">"Scalable emulation of protein equilibrium ensembles with generative deep learning"</a> biorxiv 2024 <a href="#fnref2" title="Jump back to footnote 2 in the text">↩</a></li>
+          <li id="fn3">Jing et al <a href="https://doi.org/10.48550/arXiv.2402.04845">"AlphaFold Meets Flow Matching for Generating Protein Ensembles"</a> ICML 2024 <a href="#fnref3" title="Jump back to footnote 3 in the text">↩</a></li>
+          <li id="fn4">Lu et al <a href="https://doi.org/10.48550/arXiv.2306.03117">"Str2Str: A Score-based Framework for Zero-shot Protein Conformation Sampling"</a> ICLR 2024 <a href="#fnref4" title="Jump back to footnote 4 in the text">↩</a></li>
+          <li id="fn5">Vani et al <a href="https://doi.org/10.1021/acs.jcim.3c01436">"Exploring Kinase Asp-Phe-Gly (DFG) Loop Conformational Stability with AlphaFold2-RAVE"</a> JCIM 2023 <a href="#fnref5" title="Jump back to footnote 5 in the text">↩</a></li>
+          <li id="fn6">de Silva et al <a href="https://doi.org/10.1038/s41467-024-46715-9">"High-throughput prediction of protein conformational distributions with subsampled AlphaFold2"</a> Nature Comms 2024 <a href="#fnref6" title="Jump back to footnote 6 in the text">↩</a></li>
+          <li id="fn7">Janson et al <a href="https://doi.org/10.1101/2025.03.09.642148">"Deep generative modeling of temperature-dependent structural ensembles of proteins"</a> biorxiv 2025 <a href="#fnref7" title="Jump back to footnote 7 in the text">↩</a></li>
+          <li id="fn8">Jumper et al <a href="https://doi.org/10.1038/s41586-021-03819-2">"Highly accurate protein structure prediction with AlphaFold"</a> Nature 2021 <a href="#fnref8" title="Jump back to footnote 8 in the text">↩</a></li>
+          <li id="fn9">Lindorff-Larson et al <a href="https://doi.org/10.1126/science.1208351">"How fast-folding proteins fold"</a> Science 2011 <a href="#fnref9" title="Jump back to footnote 9 in the text">↩</a></li>
+          <li id="fn10">Mirarchi et al <a href="https://doi.org/10.1038/s41597-024-04140-z">"mdCATH: A Large-Scale MD Dataset for Data-Driven Computational Biophysics"</a> Scientific Data 2024 <a href="#fnref10" title="Jump back to footnote 10 in the text">↩</a></li>
+        </ol>
+      </section>
+    </>
+  ),
+};
