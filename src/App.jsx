463bd5aa8e06447b4a0df27ae2789e846994459e
delalamo <diego.delalamo@gmail.com>
2025-04-21 06:33:56 +0200

    Adding mathjax and another post

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,36 @@ import Layout from './components/Layout'; // Import the Layout component
 import HomePage from './pages/HomePage';
 import BlogPostPage from './pages/BlogPostPage';
 import CVPage from './pages/CVPage';
+import { MathJaxContext } from 'better-react-mathjax';
 
 function App() {
   return (
     <Layout>
       {' '}
       {/* Wrap all routes/pages in the Layout */}
-      <Routes>
-        {/* Route for the homepage */}
-        <Route path="/" element={<HomePage />} />
+      <MathJaxContext>
+        <Routes>
+          {/* Route for the homepage */}
+          <Route path="/" element={<HomePage />} />
 
-        {/* Route for individual blog posts */}
-        {/* ':postId' is a URL parameter that we can access in BlogPostPage */}
-        <Route path="/post/:postId" element={<BlogPostPage />} />
+          {/* Route for individual blog posts */}
+          {/* ':postId' is a URL parameter that we can access in BlogPostPage */}
+          <Route path="/post/:postId" element={<BlogPostPage />} />
 
-        {/* Route for the about page */}
-        <Route path="/CV" element={<CVPage />} />
+          {/* Route for the about page */}
+          <Route path="/CV" element={<CVPage />} />
 
-        {/* Optional: Add a catch-all 404 route */}
-        <Route
-          path="*"
-          element={
-            <div>
-              <h2>404 - Page Not Found</h2>
-            </div>
-          }
-        />
-      </Routes>
+          {/* Optional: Add a catch-all 404 route */}
+          <Route
+            path="*"
+            element={
+              <div>
+                <h2>404 - Page Not Found</h2>
+              </div>
+            }
+          />
+        </Routes>
+      </MathJaxContext>
     </Layout>
   );
 }
