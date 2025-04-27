f9df8f9be8c6492c34b3d4f2b2366d9d3cf2211b
delalamo <diego.delalamo@gmail.com>
2025-04-27 16:14:29 +0200

    Adding PPT

---
+++ src/blog-posts/content/2025-04-27-denovo-dynamics.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const post = {
+  id: '2025-04-27-de-novo-dynamics-prediction',
+  title: 'De novo prediction of protein structural dynamics',
+  date: '2025-04-27',
+  category: 'Bio/ML',
+  excerpt: 'An overview of the state of the art that I will present on 28 April 2025',
+  content: (
+    <>
+      <p>On Monday, 28 April 2025, I'll be presenting this overview of the state of <em>de novo</em> prediction of protein structural dynamics. Note that since it is a PDF, the animations were lost.</p> 
+
+
+      <iframe
+        src="/assets/post_images/2025_04_27_PPT.pdf"
+        width="100%" // Or fixed width like "800px"
+        height="600px" // Set a desired height
+        style={{ border: '1px solid #ccc' }} // Optional styling
+        title="Embedded PDF Document" // Accessibility
+      >
+        Your browser does not support embedded PDFs. You can{' '}
+        <a href="/assets/post_images/2025_04_27_PPT.pdf">download the PDF</a> instead.
+      </iframe>
+    </>
+  ),
+};
