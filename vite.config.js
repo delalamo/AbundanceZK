f35d7fa5733b095f69775e9366a899a55f2cd7b1
delalamo <diego.delalamo@gmail.com>
2025-05-11 18:55:21 +0200

    Updating vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true, // Automatically opens the app in the browser
+  },
+  build: {
+    rollupOptions: {
+      input: '/index.html', // Ensure index.html is the entry point
+    },
+  },
 });
